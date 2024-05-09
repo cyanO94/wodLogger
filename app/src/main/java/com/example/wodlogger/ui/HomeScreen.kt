2338d7ea074d999f91b75a9f322c8246d9e5a58b
cyanO94 <dhdnjscks@gmail.com>
2024-05-09 23:28:43 +0900

    Add Sample

--- app/src/main/java/com/example/wodlogger/ui/HomeScreen.kt
+++ app/src/main/java/com/example/wodlogger/ui/HomeScreen.kt
@@ -1,20 +1,38 @@
 package com.example.wodlogger.ui
 
-import android.graphics.Color
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.wodlogger.ui.theme.WodLoggerTheme
 import com.kizitonwose.calendar.compose.HorizontalCalendar
 import com.kizitonwose.calendar.compose.rememberCalendarState
 import com.kizitonwose.calendar.core.CalendarDay
-import com.kizitonwose.calendar.core.firstDayOfWeekFromLocale
+import com.kizitonwose.calendar.core.DayPosition
+import com.kizitonwose.calendar.core.daysOfWeek
+import java.time.DayOfWeek
 import java.time.YearMonth
+import java.time.format.TextStyle
+import java.util.Locale
 
 
 @Composable
@@ -22,18 +40,85 @@ internal fun HomeScreen() {
     val currentMonth = remember { YearMonth.now() }
     val startMonth = remember { currentMonth.minusMonths(12 * 5) }
     val endMonth = remember { currentMonth.plusMonths(12) }
-    val firstDayOfWeek = remember { firstDayOfWeekFromLocale() }
+    val daysOfWeek = remember { daysOfWeek() }
+
     val state = rememberCalendarState(
         startMonth = startMonth,
         endMonth = endMonth,
         firstVisibleMonth = currentMonth,
-        firstDayOfWeek = firstDayOfWeek
+        firstDayOfWeek = daysOfWeek.first()
     )
 
-    HorizontalCalendar(
-        state = state,
-        dayContent = { Day(it) }
-    )
+    Column(Modifier.padding(vertical = 16.dp)) {
+        DaysOfWeekTitle(daysOfWeek)
+        Spacer(modifier = Modifier.height(16.dp))
+        HorizontalCalendar(
+            state = state,
+            dayContent = { Day(it) }
+        )
+
+        WodItem()
+    }
+
+}
+
+@Composable
+internal fun WodItem() {
+    Column {
+        WodItemTitle()
+        WodItemDetail()
+    }
+}
+
+@Composable
+internal fun WodItemDetailTitle() {
+    Row {
+        Text("" )
+        Text("" )
+        Text("Count" )
+        Text("Count" )
+        Text("Count" )
+    }
+}
+
+@Composable
+internal fun WodItemDetail() {
+    // item // my
+    //WodItemDetailTitle()
+    Text("150 Wall ball(20/14)")
+}
+
+
+
+@Composable
+internal fun WodItemTitle() {
+    Row(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Icon(
+            modifier = Modifier.size(32.dp, 32.dp),
+            imageVector = Icons.Default.Face,
+            contentDescription = null
+        )
+        Spacer(Modifier.width(16.dp))
+        Text("ForTime", fontSize = 32.sp)
+        Spacer(modifier = Modifier.weight(1f))
+        Text("my Result : 9:29")
+    }
+}
+
+
+@Composable
+fun DaysOfWeekTitle(daysOfWeek: List<DayOfWeek>) {
+    Row(modifier = Modifier.fillMaxWidth()) {
+        for (dayOfWeek in daysOfWeek) {
+            Text(
+                modifier = Modifier.weight(1f),
+                textAlign = TextAlign.Center,
+                text = dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.getDefault()),
+            )
+        }
+    }
 }
 
 @Composable
@@ -43,7 +128,10 @@ internal fun Day(day: CalendarDay) {
             .aspectRatio(1f), // This is important for square sizing!
         contentAlignment = Alignment.Center
     ) {
-        Text(text = day.date.dayOfMonth.toString())
+        Text(
+            text = day.date.dayOfMonth.toString(),
+            color = if (day.position == DayPosition.MonthDate) Color(0xFF000000) else Color(0xFF888888),
+        )
     }
 }
 
